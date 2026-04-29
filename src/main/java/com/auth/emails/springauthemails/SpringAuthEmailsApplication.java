0191ba5867110fba53a4d257d5ae2f72599379d4
vtasun <tasun.v.s@gmail.com>
2026-04-29 22:20:16 +0300

    add missing files

--- src/main/java/com/auth/emails/springauthemails/SpringAuthEmailsApplication.java
+++ src/main/java/com/auth/emails/springauthemails/SpringAuthEmailsApplication.java
@@ -2,12 +2,14 @@ package com.auth.emails.springauthemails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringAuthEmailsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAuthEmailsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthEmailsApplication.class, args);
+    }
 
 }
--- src/test/java/com/auth/emails/springauthemails/SpringAuthEmailsApplicationTests.java
+++ src/test/java/com/auth/emails/springauthemails/SpringAuthEmailsApplicationTests.java
@@ -1,13 +1,4 @@
 package com.auth.emails.springauthemails;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringAuthEmailsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
